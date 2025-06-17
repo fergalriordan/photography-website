f72257f6fc6c38b8378f0be5d71a62d282fbc805
Fergal Riordan <fergalriordan333@gmail.com>
2025-06-17 22:52:57 +0100

    Implement home page collage

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,15 @@
 
 export const SITE_TITLE = 'Fergal Riordan | Photography & Travel';
 export const SITE_DESCRIPTION = 'Travel, nature, photography, blogs and more.';
+
+export const HERO_PHOTOS = [
+  { src: 'src/assets/laguna69.jpg', alt: 'Blue glacial lake in the Andes (Laguna 69)' },  
+  { src: 'src/assets/hammocks_amazon_slow_boat.jpg', alt: 'Row of hammocks on an Amazon river slow boat' },
+  { src: 'src/assets/cows_and_valley_laguna69_trek.jpg', alt: 'Cows grazing in valley below Laguna 69 hike' },  
+  { src: 'src/assets/mules_salkantay_pass.jpg', alt: 'Pack mules on the Salkantay trail in Peru' },
+  { src: 'src/assets/hammocks_close_up.jpg', alt: 'Close-up of hammocks on a riverboat' },
+  { src: 'src/assets/salento_palm_tree.jpg', alt: 'Tall palm trees in Salento, Colombia' },
+  { src: 'src/assets/cargo_area_amazon_slow_boat.jpg', alt: 'Cargo area of Amazon slow boat with supplies' },
+  { src: 'src/assets/river_salkantay_trek.jpg', alt: 'Mountain river along the Salkantay trek' },
+  { src: 'src/assets/cargo_loading_amazon_slow_boat.jpg', alt: 'Workers loading cargo onto an Amazon riverboat' },
+];
