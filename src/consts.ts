3b465a0d173897aa68bd5dd158da6034579d37cb
Fergal Riordan <fergalriordan333@gmail.com>
2025-06-17 23:42:45 +0100

    Update site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Fergal Riordan | Photography & Travel';
+export const SITE_TITLE = 'Fergal Riordan | Travel, Nature, Photography';
 export const SITE_DESCRIPTION = 'Travel, nature, photography, blogs and more.';
 
 export const HERO_PHOTOS = [
