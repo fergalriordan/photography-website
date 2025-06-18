d92e73cbf633f26250ca91b5cb641d8102414924
Fergal Riordan <fergalriordan333@gmail.com>
2025-06-18 16:20:57 +0100

    Add tagline

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Fergal Riordan | Travel, Nature, Photography';
+export const SITE_TITLE = 'Fergal Riordan';
+export const SITE_TAGLINE = 'Travel, Nature, Photography';
 export const SITE_DESCRIPTION = 'Travel, nature, photography, blogs and more.';
 
 export const HERO_PHOTOS = [
