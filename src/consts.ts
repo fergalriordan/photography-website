8e167da88670d0a5dae8e1b73a23a86373307e02
Fergal Riordan <fergalriordan333@gmail.com>
2025-07-08 16:37:24 +0100

    Remove duplicate images

--- src/consts.ts
+++ src/consts.ts
@@ -32,7 +32,6 @@ export const HOMEPAGE_COLLAGE_HERO_PHOTOS = [
 
 export const ADDITIONAL_HOMEPAGE_COLLAGE_PHOTOS = [
   
-  { src: 'src/assets/home_page_collage_lazy/tikal_spider_monkeys.jpg', alt: 'Spider monkeys in Tikal, Mexico' },
   { src: 'src/assets/home_page_collage_lazy/cusco_view.JPG', alt: 'View over Cusco rooftops and hills' },
   { src: 'src/assets/home_page_collage_lazy/landscape_machu_picchu_trek.JPG', alt: 'Mountain landscape on the Machu Picchu trek' },
   { src: 'src/assets/home_page_collage_lazy/pineapple_mural.jpg', alt: 'Mural of anthropomorphised pineapple in Baccalar, Mexico' },
@@ -43,8 +42,8 @@ export const ADDITIONAL_HOMEPAGE_COLLAGE_PHOTOS = [
   { src: 'src/assets/home_page_collage_lazy/antigua_arches.JPG', alt: 'Arches in town centre in Antigua, Guatemala' },
   { src: 'src/assets/home_page_collage_lazy/coca_cola_shops.jpg', alt: 'Shops covered in Coca-Cola branding in Baccalar, Mexico' },
   { src: 'src/assets/home_page_collage_lazy/cargo_loading_amazon_slow_boat.JPG', alt: 'Cargo area of Amazon slow boat with supplies' },
+  { src: 'src/assets/home_page_collage_lazy/el_salvador_chicken_bus.JPG', alt: 'Chicken bus in El Salvador' },
   { src: 'src/assets/home_page_collage_lazy/vestrahorn.JPG', alt: 'Vestrahorn landscape shot' },
-  { src: 'src/assets/home_page_collage_lazy/amazon_slow_boat_window.JPG', alt: 'View through a window on the Amazon slow boat' },
   { src: 'src/assets/home_page_collage_lazy/hengifoss_waterfalls.jpg', alt: 'The two waterfalls on the Hengifoss trail' },
   { src: 'src/assets/home_page_collage_lazy/laguna69_trek_landscape.JPG', alt: 'Panoramic view of the Laguna 69 trekking path' },
   { src: 'src/assets/home_page_collage_lazy/amazon_boat_side.JPG', alt: 'Side profile of an Amazon riverboat cruising downstream' },
@@ -58,7 +57,6 @@ export const ADDITIONAL_HOMEPAGE_COLLAGE_PHOTOS = [
   { src: 'src/assets/home_page_collage_lazy/cusco_ruins_landscape.JPG', alt: 'Wide view of Incan ruins outside Cusco, Peru' },
   { src: 'src/assets/home_page_collage_lazy/laguna69_trek_view.JPG', alt: 'High-altitude viewpoint on the Laguna 69 hike' },
   { src: 'src/assets/home_page_collage_lazy/iceland_sheep.JPG', alt: 'Sheep near Studlagil canyon in Iceland' },
-  { src: 'src/assets/home_page_collage_lazy/el_salvador_chicken_bus.JPG', alt: 'Chicken bus in El Salvador' },
   { src: 'src/assets/home_page_collage_lazy/iceland_road_trip_pit_stop.JPG', alt: 'Road trip pit stop in Iceland' },
   { src: 'src/assets/home_page_collage_lazy/laguna69.JPG', alt: 'Laguna 69 in Huaraz, Peru' },
   { src: 'src/assets/home_page_collage_lazy/machu_picchu_viewpoint.JPG', alt: 'Viewpoint overlooking Machu Picchu' },
