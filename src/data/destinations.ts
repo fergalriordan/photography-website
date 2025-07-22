46738a44a3ce636d71e828592345fb614ce28c2c
Fergal Riordan <fergalriordan333@gmail.com>
2025-07-22 12:02:14 +0100

    Move consts into new data folder and split into separate files

---
+++ src/data/destinations.ts
@@ -0,0 +1,33 @@
+// Destinations Hero Photos
+
+export const DESTINATIONS_ICELAND_HERO_PHOTOS = [
+  { src: 'src/assets/DestinationsHeroPhotos/iceland/iceland_sheep.jpg', alt: 'Sheep in Iceland' },
+  { src: 'src/assets/DestinationsHeroPhotos/iceland/IMG_7826.jpg', alt: 'Icelandic horse grazing' },
+  { src: 'src/assets/DestinationsHeroPhotos/iceland/IMG_7843.jpg', alt: 'Studlagil canyon iceland' },
+  { src: 'src/assets/DestinationsHeroPhotos/iceland/east_fjords_fishing_boat.jpg', alt: 'Fishing boat in the east of iceland' },
+  { src: 'src/assets/DestinationsHeroPhotos/iceland/hengifoss_waterfalls.jpg', alt: 'Hengifoss waterfalls in Iceland' },
+];
+
+export const DESTINATIONS_MESOAMERICA_HERO_PHOTOS = [
+  { src: 'src/assets/DestinationsHeroPhotos/mesoamerica/IMG_5953.jpg', alt: 'Boat in front of volcano in Lake Atitlan, Guatemala' },
+  { src: 'src/assets/DestinationsHeroPhotos/mesoamerica/baccalar_bike.jpg', alt: 'Man riding a bicycle under colourful bunting' },
+  { src: 'src/assets/DestinationsHeroPhotos/mesoamerica/IMG_5130.jpg', alt: 'Max with Tikal ruins poking through the jungle in the background' },
+  { src: 'src/assets/DestinationsHeroPhotos/mesoamerica/IMG_5116.jpg', alt: 'Old ladies walking together in Baccalar, Mexico' },
+  { src: 'src/assets/DestinationsHeroPhotos/mesoamerica/pole_performance.jpg', alt: 'Traditional cultural performance in Baccalar, Mexico' },
+];
+
+export const DESTINATIONS_SOUTH_AMERICA_HERO_PHOTOS = [
+  { src: 'src/assets/DestinationsHeroPhotos/south_america/CNV00024.JPG', alt: 'Landscape on the Salkantay trek, Peru' },
+  { src: 'src/assets/DestinationsHeroPhotos/south_america/laguna_69_trek.JPG', alt: 'View on the Laguna 69 trek, Peru' },
+  { src: 'src/assets/DestinationsHeroPhotos/south_america/machu_picchu_viewpoint.JPG', alt: 'View on Machu Picchu trek, Peru' },
+  { src: 'src/assets/DestinationsHeroPhotos/south_america/salento_palm_tree.JPG', alt: 'Palm tree in Salento, Colombia' },
+  { src: 'src/assets/DestinationsHeroPhotos/south_america/CNV00020.JPG', alt: 'Stone stacks on Salkanty Pass, Peru' },
+];
+
+export const DESTINATIONS_AMAZON_HERO_PHOTOS = [
+  { src: 'src/assets/DestinationsHeroPhotos/amazon/CNV00027.JPG', alt: 'Cargo loading area on Amazon slow boat' },
+  { src: 'src/assets/DestinationsHeroPhotos/amazon/CNV00010.JPG', alt: 'Captain\'s cabin on Amazon slow boat' },
+  { src: 'src/assets/DestinationsHeroPhotos/amazon/__CNV00008.JPG', alt: 'hammocks on Amazon slow boat' },
+  { src: 'src/assets/DestinationsHeroPhotos/amazon/CNV00030.JPG', alt: 'Side view of Amazon slow boat' },
+  { src: 'src/assets/DestinationsHeroPhotos/amazon/CNV00031__.JPG', alt: 'Large old tree in the Amazon' },
+];
---
+++ src/data/site.ts
@@ -0,0 +1,5 @@
+// Site Metadata
+
+export const SITE_TITLE = 'Fergal Riordan';
+export const SITE_TAGLINE = 'Travel, Nature, Photography';
+export const SITE_DESCRIPTION = 'Travel, nature, photography, blogs and more.';
